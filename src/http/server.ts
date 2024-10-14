1c0d5e2b7b3dd84c92cbe538919a18394fd98451
Felipe Botelho <felipebotelhofelipe@hotmail.com>
2024-10-14 19:02:40 -0300

    Installed dependencies and Eslint configuration

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,25 @@
-import {Elysia} from 'elysia'
+import cors from '@elysiajs/cors'
+import { Elysia } from 'elysia'
 
 const app = new Elysia()
-.get('/',()=>{
+  .use(
+    cors({
+      credentials: true,
+      allowedHeaders: ['content-type'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+      origin: (request): boolean => {
+        const origin = request.headers.get('origin')
+        if (!origin) {
+          return false
+        }
+        return true
+      },
+    }),
+  )
+  .get('/', () => {
     return 'Hello World'
-})
+  })
 
-app.listen(3333,()=>{
-    console.log("Http server running!")
-});
+app.listen(3333, () => {
+  console.log('Http server running!')
+})
